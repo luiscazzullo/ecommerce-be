999fe63cd92223130e2a735a5e9a3bfc5e7cdb7e
FranMerchan <merchanf6@gmail.com>
2021-09-11 15:13:05 -0300

    problemas de cors resueltos para el login

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const authRoutes = require("./routes/authRoutes");
+const cors = require("cors")
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use("/api/v1/auth", authRoutes);
 
